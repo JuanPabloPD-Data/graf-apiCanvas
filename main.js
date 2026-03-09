071e5f73ec2d2b6be6638ae079d4b94c226f2170
JuanPabloPD-Data <juanpablopedi12@gmail.com>
2026-03-09 04:04:53 -0600

    Version inicial

---
+++ main.js
@@ -0,0 +1,10 @@
+function draw() {
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+  
+      ctx.fillStyle = "green";
+      ctx.fillRect(10, 10, 100, 100);
+      
+    }
+  }
