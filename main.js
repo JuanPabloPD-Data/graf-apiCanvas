7558d89b5d83346ddc4b183c34735ced3e997464
JuanPabloPD-Data <juanpablopedi12@gmail.com>
2026-03-09 04:24:04 -0600

    Creacion Lineas

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ function draw() {
       const ctx = canvas.getContext("2d");
   
       
-      ctx.beginPath();
-      ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Círculo externo
-      ctx.moveTo(110, 75);
-      ctx.arc(75, 75, 35, 0, Math.PI, false); // Boca (en el sentido de las agujas del reloj)
-      ctx.moveTo(65, 65);
-      ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Ojo izquierdo
-      ctx.moveTo(95, 65);
-      ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Ojo derecho
-      ctx.stroke();
+    // Triángulo relleno
+    ctx.beginPath();
+    ctx.moveTo(25, 25);
+    ctx.lineTo(105, 25);
+    ctx.lineTo(25, 105);
+    ctx.fill();
+
+    // Triángulo contorneado
+    ctx.beginPath();
+    ctx.moveTo(125, 125);
+    ctx.lineTo(125, 45);
+    ctx.lineTo(45, 125);
+    ctx.closePath();
+    ctx.stroke();
       
     }
   }
