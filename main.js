7670220e1c68cbdb08f598850fb22aa9d4d48d64
JuanPabloPD-Data <juanpablopedi12@gmail.com>
2026-03-09 04:10:58 -0600

    Creacion Triangulo

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ function draw() {
       const ctx = canvas.getContext("2d");
   
       ctx.fillStyle = "green";
-      ctx.fillRect(50, 50, 250, 100)
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fill();
       
     }
   }
